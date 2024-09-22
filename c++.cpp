2a478ed8ad12b0db4d0eb5659f754472802546d6
Mohd juned Khan <118798558+juned-k786@users.noreply.github.com>
2024-09-22 11:25:40 +0530

    updated

--- c++.cpp
+++ c++.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 bool isSafe(int** arr, int x, int y, int n){
-    //check coln any queen is present or not
+    
     for(int row=0;row<x;row++){
-        //If queen is present it shows 1 
+         
         if(arr[row][y]==1){
             return false;
         }
         
     }
-    //check for left daigonal
+    
     int row=x;
     int col=y;
     while(row>=0  && col>=0){
@@ -19,7 +19,7 @@ bool isSafe(int** arr, int x, int y, int n){
         row--;
         col--;
     } 
-    //check for right daigonal
+    
     row=x;
     col=y;
     while(row>=0  && col<n){
@@ -31,34 +31,32 @@ bool isSafe(int** arr, int x, int y, int n){
     }
     return true;
 }
-//recursive backtracking
 bool nQueen(int** arr, int x, int n){
     if(x>=n){
         return true;
     }
-    //checkimg queen is safe in that position or not 
     for(int col=0;col<n;col++){
-    //check is safe
+
         if(isSafe(arr,x,col,n)){
-            //if true then place 1
-            arr[x][col]=1; // place a queen
+            
+            arr[x][col]=1; 
             
             if(nQueen(arr,x+1,n)){
-                return true; //move to next row if successful
+                return true;
             }
-            //if not giving true and use backtracking
-            arr[x][col]=0; //backtracking step (remove the queen if not placing)
+        
+            arr[x][col]=0; 
         }
     }
-    //not able to place queen in any col
-    return false; //no coln is safe (backtrack)
+    
+    return false;
     
 }
 
 int main() {
     int n;
     cin>>n;
-    //making 2d array
+    
     int** arr=new int*[n];
     for(int i=0;i<n;i++){
         arr[i]=new int[n];
@@ -74,6 +72,5 @@ int main() {
             cout<<endl;
         }
     }
-    //if  no soln is possible 
     return 0;
 }
